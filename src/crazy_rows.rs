612d91d2c8c29979c0ed0a277fc820521402b789
Unknown <nannyakannya@gmail.com>
2018-07-11 17:27:25 +0900

    fix

--- src/crazy_rows.rs
+++ src/crazy_rows.rs
@@ -6,27 +6,26 @@ fn main() {
     rows.push(vec![1, 0, 0, 0]);
 
     let mut a:Vec<i32> = vec![0; rows.len()];
-    for (i, row) in rows.into_iter().enumerate() {
+    for (i, row) in rows.iter().enumerate() {
         a[i] = -1;
-        for (j, num) in row.into_iter().enumerate() {
-            if num == 1 {
+        for (j, num) in row.iter().enumerate() {
+            if *num == 1 {
                 a[i] = j as i32;
             }
         }
     }
     let mut res = 0;
+
     for i in 0..rows.len() {
         let mut pos:usize = 0;
         for j in i..rows.len() {
-            let i = i as i32;
-            if a[j] <= i {
+            if a[j] <= i as i32 {
                 pos = j;
                 break;
             }
         }
-        for j in pos..i {
-            let j = j as usize;
-            std::mem::swap(&mut a[j], &mut a[j-1]);
+        for j in (i+1..pos+1).rev() {
+            a.swap(j, j - 1);
             res += 1;
         }
     }
