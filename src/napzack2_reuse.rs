1251ddb8eeb1ccb5984d944f38d81e380c3bffec
Unknown <nannyakannya@gmail.com>
2018-08-04 14:18:17 +0900

    napzack2_reuse

---
+++ src/napzack2_reuse.rs
@@ -0,0 +1,23 @@
+fn solve(w: &Vec<usize>, v: &Vec<usize>, max_w: usize) -> usize {
+    let mut dp: Vec<usize> = vec![0; 10];
+    for i in 0..w.len() {
+        let i: usize = w.len() - 1 - i;
+        for j in 0..max_w + 1 {
+            if j < w[i] {
+                dp[j] = dp[j];
+            } else {
+                dp[j] = std::cmp::max(dp[j], dp[j - w[i]] + v[i]);
+                println!("{0}, {1}, {2}", i, j, dp[j]);
+            }
+        }
+    }
+    return dp[max_w];
+}
+
+fn main() {
+    let w: Vec<usize> = vec![2, 1, 3, 2];
+    let v: Vec<usize> = vec![3, 2, 4, 2];
+
+    let ans = solve(&w, &v, 5);
+    println!("{:?}", ans);
+}
