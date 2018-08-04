1d3fbb027fe7184563bdb95dbf610aa4376dcdd7
Unknown <nannyakannya@gmail.com>
2018-08-04 14:03:16 +0900

    refactoring

--- src/napzack2.rs
+++ src/napzack2.rs
@@ -1,36 +1,23 @@
-static mut N: usize = 0;
-static W: usize = 5;
-
-fn rec(w: &Vec<usize>, v: &Vec<usize>, dp: &mut Vec<Vec<usize>>) -> usize {
-    unsafe {
-        for i in 0..N {
-            let i:usize = N - 1 - i;
-            for j in 0..W+1 {
-                if j < w[i] {
-                    dp[i][j] = dp[i + 1][j];
-                } else {
-                    dp[i][j] = std::cmp::max(dp[i + 1][j], dp[i + 1][j - w[i]] + v[i]);
-                    println!("{0}, {1}, {2}", i, j, dp[i][j]);
-                }
+fn solve(w: &Vec<usize>, v: &Vec<usize>, max_w: usize) -> usize {
+    let mut dp: Vec<Vec<usize>> = vec![vec![0; 10]; 10];
+    for i in 0..w.len() {
+        let i: usize = w.len() - 1 - i;
+        for j in 0..max_w + 1 {
+            if j < w[i] {
+                dp[i][j] = dp[i + 1][j];
+            } else {
+                dp[i][j] = std::cmp::max(dp[i + 1][j], dp[i + 1][j - w[i]] + v[i]);
+                println!("{0}, {1}, {2}", i, j, dp[i][j]);
             }
         }
     }
-    return dp[0][W];
+    return dp[0][max_w];
 }
 
-
 fn main() {
-    unsafe {
-        N = 4;
-    }
-    let w: Vec<usize> = vec![2 ,1 ,3, 2];
+    let w: Vec<usize> = vec![2, 1, 3, 2];
     let v: Vec<usize> = vec![3, 2, 4, 2];
-    let mut dp: Vec<Vec<usize>> = Vec::new();
-    for _i in 0..10 {
-        let row: Vec<usize> = vec![0; 10];
-        dp.push(row);
-    }
 
-    let ans = rec(&w, &v, &mut dp);
+    let ans = solve(&w, &v, 5);
     println!("{:?}", ans);
 }
