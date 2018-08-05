d9238e65c4c6c1a976bfb40e4c99023f3962c1da
fukatani <nannyakannya@gmail.com>
2018-08-05 17:07:44 +0900

    add reverse

--- src/lib.rs
+++ src/lib.rs
@@ -120,6 +120,20 @@ fn argmax<T>(u: &[T]) -> (usize)
     max_index
 }
 
+#[derive(Eq, PartialEq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<std::cmp::Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> std::cmp::Ordering {
+        other.0.cmp(&self.0)
+    }
+}
 
 fn main() {
     let nums: Vec<i32> = read_vec();
