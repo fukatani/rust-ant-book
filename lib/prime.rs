5b1fc8dcd7ad3fb559b6c1e3ca26d00868d7196c
Unknown <nannyakannya@gmail.com>
2019-04-19 20:54:02 +0900

    added factorize

--- lib/prime.rs
+++ lib/prime.rs
@@ -1,11 +1,10 @@
-fn main() {
-    let n = 20000;
-    let mut is_prime = vec![true; n];
+fn get_primes(n: usize) -> Vec<usize> {
+    let mut is_prime = vec![true; n + 1];
     let mut primes = Vec::new();
     is_prime[0] = false;
     is_prime[1] = false;
 
-    for i in 2..n {
+    for i in 2..n + 1 {
         if is_prime[i] {
             primes.push(i);
             let mut j = 2 * i;
@@ -15,5 +14,24 @@ fn main() {
             }
         }
     }
-    println!("{:?}", primes);
+    primes
+}
+
+fn factorize(mut num: usize, primes: &Vec<usize>) -> std::collections::HashMap<usize, usize>  {
+    let mut dict = std::collections::HashMap::new();
+    for &p in primes.iter() {
+        while num % p == 0 {
+            *dict.entry(p).or_insert(0) += 1usize;
+            num /= p;
+        }
+        if num == 1 {
+            break;
+        }
+    }
+    dict
+}
+
+fn main() {
+    println!("{:?}", get_primes(20));
+    println!("{:?}", factorize(10, &get_primes(20)));
 }
