c39b7d298d993722b96c358d45984de7b74a3523
fukatani <nannyakannya@gmail.com>
2019-06-01 20:42:53 +0900

    adjacent

--- src/adjacent_graph_sparse.rs
+++ src/adjacent_graph_sparse.rs
@@ -4,20 +4,20 @@ struct AdjacentMatrix {
 
 impl AdjacentMatrix {
     pub fn new(n: usize) -> AdjacentMatrix {
-        AdjacentMatrix{
-            mat: vec![vec![0; n]; n],
+        AdjacentMatrix {
+            mat: vec![vec![]; n],
         }
     }
 
     pub fn connect(&mut self, x: usize, y: usize) {
-        self.mat[x][y] = 1;
-        self.mat[y][x] = 1;
+        self.mat[x].push(y);
+        self.mat[y].push(x);
     }
 }
 
 struct Solver {
     adj_mat: AdjacentMatrix,
-    colors: Vec<i32>  // 0 uncolored, 1 or -1
+    colors: Vec<i32>,
 }
 
 impl Solver {
@@ -26,31 +26,18 @@ impl Solver {
             return false;
         }
         self.colors[idx] = color;
-        println!("{0} to {1}", idx, color);
-        for i in 0..self.adj_mat.mat[idx].len() {
-            if self.adj_mat.mat[idx][i] == 0 {
-                continue;
-            }
+        let adjacents = self.adj_mat.mat[idx].iter().cloned().collect::<Vec<_>>();
+        for i in adjacents {
             if self.colors[i] == 0 {
                 let result = self.solve(i, color * -1);
                 if !result {
                     return false;
                 }
             } else if self.colors[i] == color {
+                self.colors[i] = 2;
                 return false;
             }
         }
         true
     }
-}
-
-fn main() {
-    let mut adj_mat = AdjacentMatrix::new(4);
-    adj_mat.connect(0, 1);
-    adj_mat.connect(0, 3);
-    adj_mat.connect(1, 2);
-    adj_mat.connect(3, 2);
-    let colors = vec![0; 4];
-    let mut solver = Solver{adj_mat: adj_mat, colors: colors};
-    println!("{:?}", solver.solve(0, 1));
 }
