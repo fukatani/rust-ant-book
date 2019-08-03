bac594c80b6012af484fbc80caa17c14f17f7d74
fukatani <nannyakannya@gmail.com>
2019-08-03 20:11:29 +0900

    hash

--- src/rolling_hash_contains.rs
+++ src/rolling_hash_contains.rs
@@ -1,3 +1,4 @@
+use std::cmp::*;
 use std::num::Wrapping;
 
 const big_int: Wrapping<u64> = Wrapping(1000_000_000 + 7);
@@ -34,6 +35,25 @@ fn contains(a: &Vec<u64>, b: &Vec<u64>) -> bool {
     false
 }
 
+fn overlap(a: &Vec<u64>, b: &Vec<u64>) -> usize {
+    let a = a.iter().map(|&x| Wrapping(x)).collect::<Vec<_>>();
+    let b = b.iter().map(|&x| Wrapping(x)).collect::<Vec<_>>();
+
+    let mut ans = 0;
+    let mut ah = Wrapping(0u64);
+    let mut bh = Wrapping(0u64);
+    let mut t = Wrapping(1u64);
+    for i in 1..min(a.len(), b.len()) + 1 {
+        ah = ah + a[a.len() - i] * t;
+        bh = bh * big_int + b[i - 1];
+        if ah == bh {
+            ans = i;
+        }
+        t *= big_int;
+    }
+    ans
+}
+
 fn main() {
     // let a = "earn".chars().map(|x| x as u64).collect::<Vec<_>>();
     let a = "earn".chars().map(|x| x as u64).collect::<Vec<_>>();
@@ -42,5 +62,8 @@ fn main() {
     let a = "earm".chars().map(|x| x as u64).collect::<Vec<_>>();
     let b = "learning".chars().map(|x| x as u64).collect::<Vec<_>>();
     assert_eq!(contains(&a, &b), false);
-}
 
+    let a = "deep".chars().map(|x| x as u64).collect::<Vec<_>>();
+    let b = "epsilon".chars().map(|x| x as u64).collect::<Vec<_>>();
+    assert_eq!(overlap(&a, &b), 2);
+}
