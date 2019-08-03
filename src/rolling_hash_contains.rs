9f4cab2622a632882da4b984bc22d5a3af515733
fukatani <nannyakannya@gmail.com>
2019-08-03 20:01:56 +0900

    rolling hash contains

---
+++ src/rolling_hash_contains.rs
@@ -0,0 +1,46 @@
+use std::num::Wrapping;
+
+const big_int: Wrapping<u64> = Wrapping(1000_000_000 + 7);
+
+// b contains a?
+fn contains(a: &Vec<u64>, b: &Vec<u64>) -> bool {
+    if a.len() > b.len() {
+        return false;
+    }
+    let a = a.iter().map(|&x| Wrapping(x)).collect::<Vec<_>>();
+    let b = b.iter().map(|&x| Wrapping(x)).collect::<Vec<_>>();
+    let mut power_base = Wrapping(1u64);
+    for _ in 0..a.len() {
+        power_base *= big_int;
+    }
+    let mut ah = Wrapping(0u64);
+    let mut bh = Wrapping(0u64);
+    for i in 0..a.len() {
+        ah = ah * big_int + a[i];
+    }
+    for i in 0..a.len() {
+        bh = bh * big_int + b[i];
+    }
+
+    for i in 0..a.len() {
+        if ah == bh {
+            return true;
+        }
+        if i + a.len() >= b.len() {
+            break;
+        }
+        bh = bh * big_int + b[i + a.len()] - b[i] * power_base;
+    }
+    false
+}
+
+fn main() {
+    // let a = "earn".chars().map(|x| x as u64).collect::<Vec<_>>();
+    let a = "earn".chars().map(|x| x as u64).collect::<Vec<_>>();
+    let b = "learning".chars().map(|x| x as u64).collect::<Vec<_>>();
+    assert_eq!(contains(&a, &b), true);
+    let a = "earm".chars().map(|x| x as u64).collect::<Vec<_>>();
+    let b = "learning".chars().map(|x| x as u64).collect::<Vec<_>>();
+    assert_eq!(contains(&a, &b), false);
+}
+
