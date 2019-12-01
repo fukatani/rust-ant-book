ea2dac04497395f65a318dbb5f216308c6a8dc0e
Unknown <nannyakannya@gmail.com>
2019-12-01 10:56:11 +0900

    fix ternary search

--- lib/binary_search.rs
+++ lib/binary_search.rs
@@ -15,19 +15,32 @@ where
     (ok, ng)
 }
 
-fn ternary_search<F, T: std::cmp::Ord>(lb: usize, ub: usize, criterion: F) -> usize
+fn ternary_search<F>(lb: usize, ub: usize, criterion: F) -> usize
 where
-    F: Fn(usize) -> T,
+    F: Fn(usize) -> usize,
 {
     let mut lb = lb;
     let mut ub = ub;
+    let mid = (lb + ub) / 2;
+    let mut convex_upward = false;
+    if criterion(mid) > (criterion(lb) + criterion(ub - 1)) / 2 {
+        convex_upward = true;
+    }
     while ub - lb > 2 {
         let mid1 = (ub + lb * 2) / 3;
         let mid2 = (ub * 2 + lb) / 3;
-        if criterion(mid1) < criterion(mid2) {
-            lb = mid1;
+        if convex_upward {
+            if criterion(mid1) < criterion(mid2) {
+                lb = mid1;
+            } else {
+                ub = mid2;
+            }
         } else {
-            ub = mid2;
+            if criterion(mid1) > criterion(mid2) {
+                lb = mid1;
+            } else {
+                ub = mid2;
+            }
         }
     }
     (lb + ub) / 2
@@ -36,8 +49,8 @@ where
 fn main() {
     let v = vec![2, 3, 3, 5, 6];
     let k = 4;
-    let ans = binary_search(0, v.len(), |a| v[a] >= k);
-    assert_eq!(5, v[ans]);
+    let ans = binary_search(0, v.len(), |a| v[a] <= k);
+    assert_eq!(5, v[ans.1]);
 
     let v = vec![1, 2, 3, 3, 5, 6, 4, 3, 2, 1];
     let ans = ternary_search(0, v.len(), |a| v[a]);
