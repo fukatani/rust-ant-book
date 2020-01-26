06b1fbadd11d796b28a85b1b84b339746c6567e1
fukatani <nannyakannya@gmail.com>
2020-01-26 22:23:51 +0900

    bug fix.

--- lib/lazy_eval_segtree_rsq.rs
+++ lib/lazy_eval_segtree_rsq.rs
@@ -1,8 +1,10 @@
+use std::cmp::*;
+
 #[derive(Debug)]
 struct LazySegmentTree {
     n: usize,
-    node: Vec<i64>,
-    lazy: Vec<i64>,
+    data: Vec<i64>,
+    datb: Vec<i64>,
 }
 
 impl LazySegmentTree {
@@ -13,37 +15,18 @@ impl LazySegmentTree {
         }
         LazySegmentTree {
             n: n,
-            node: vec![0i64; n * 2],
-            lazy: vec![0i64; n * 2],
-        }
-    }
-
-    fn eval(&mut self, k: usize, l: usize, r: usize) {
-        if self.lazy[k] != 0 {
-            self.node[k] += self.lazy[k];
-            if r - l > 1 {
-                self.lazy[2 * k + 1] += self.lazy[k] / 2;
-                self.lazy[2 * k + 2] += self.lazy[k] / 2;
-            }
-            self.lazy[k] = 0;
+            data: vec![0i64; 2 * n - 1],
+            datb: vec![0i64; 2 * n - 1],
         }
     }
 
     fn add(&mut self, a: usize, b: usize, x: i64, k: usize, l: usize, r: usize) {
-        self.eval(k, l, r);
-
-        // if out of range, do nothing.
-        if b <= l || r <= a {
-            return;
-        }
-
         if a <= l && r <= b {
-            self.lazy[k] += (r - l) as i64 * x;
-            self.eval(k, l, r);
-        } else {
-            self.add(a, b, x, 2 * k + 1, l, (l + r) / 2);
-            self.add(a, b, x, 2 * k + 2, (l + r) / 2, r);
-            self.node[k] = self.node[2 * k + 1] + self.node[2 * k + 2];
+            self.data[k] += x;
+        } else if l < b && a < r {
+            self.datb[k] += (min(b, r) - max(a, l)) as i64 * x;
+            self.add(a, b, x, k * 2 + 1, l, (l + r) / 2);
+            self.add(a, b, x, k * 2 + 2, (l + r) / 2, r);
         }
     }
 
@@ -51,14 +34,14 @@ impl LazySegmentTree {
         if b <= l || r <= a {
             return 0;
         }
-
-        self.eval(k, l, r);
         if a <= l && r <= b {
-            return self.node[k];
+            return self.data[k] * (r - l) as i64 + self.datb[k];
+        } else {
+            let mut res = (min(b, r) - max(a, l)) as i64 * self.data[k];
+            res += self.getsum(a, b, k * 2 + 1, l, (l + r) / 2);
+            res += self.getsum(a, b, k * 2 + 2, (l + r) / 2, r);
+            return res;
         }
-
-        self.getsum(a, b, 2 * k + 1, l, (l + r) / 2)
-            + self.getsum(a, b, 2 * k + 2, (l + r) / 2, r)
     }
 }
 
