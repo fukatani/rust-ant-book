bf8322f9846398f5934bff8749fc95d7b1387ee7
fukatani <nannyakannya@gmail.com>
2020-02-16 11:37:42 +0900

    update lca

--- lib/lca.rs
+++ lib/lca.rs
@@ -1,6 +1,6 @@
 struct LCA {
     depth: Vec<usize>,
-    parent: Vec<Vec<usize>>,
+    parent: Vec<Vec<Option<usize>>>,
     log_v: usize,
 }
 
@@ -14,10 +14,10 @@ impl LCA {
         let log_v = (1..).find(|i| 1usize << i > n).unwrap();
         let mut lca = LCA {
             depth: vec![0usize; n],
-            parent: vec![vec![std::usize::MAX; n]; 20 + 1], // support 2 ^ 20
+            parent: vec![vec![None; n]; 20 + 1], // support 2 ^ 20
             log_v: log_v,
         };
-        lca.dfs(root, std::usize::MAX, 0, &g);
+        lca.dfs(root, None, 0, &g);
         lca.doubling(g.len());
         lca
     }
@@ -25,23 +25,21 @@ impl LCA {
     fn doubling(&mut self, n: usize) {
         for k in 0..self.log_v {
             for v in 0..n {
-                if self.parent[k][v] == std::usize::MAX {
-                    self.parent[k + 1][v] = std::usize::MAX;
-                } else {
-                    self.parent[k + 1][v] = self.parent[k][self.parent[k][v]];
+                if let Some(parent_v) = self.parent[k][v] {
+                    self.parent[k + 1][v] = self.parent[k][parent_v];
                 }
             }
         }
     }
 
-    fn dfs(&mut self, cur: usize, p: usize, cur_depth: usize, g: &Vec<Vec<usize>>) {
+    fn dfs(&mut self, cur: usize, p: Option<usize>, cur_depth: usize, g: &Vec<Vec<usize>>) {
         self.parent[0][cur] = p;
         self.depth[cur] = cur_depth;
         for &to in g[cur].iter() {
-            if to == p {
+            if Some(to) == p {
                 continue;
             }
-            self.dfs(to, cur, cur_depth + 1, g);
+            self.dfs(to, Some(cur), cur_depth + 1, g);
         }
     }
 
@@ -49,21 +47,19 @@ impl LCA {
         if self.depth[u] > self.depth[v] {
             std::mem::swap(&mut u, &mut v);
         }
-        for k in 0..self.parent.len() {
-            if (self.depth[v] - self.depth[u] >> k) & 1 == 1 {
-                v = self.parent[k][v];
-            }
+        while self.depth[v] != self.depth[u] {
+            v = self.parent[(self.depth[v] - self.depth[u]).trailing_zeros() as usize][v].unwrap();
         }
         if u == v {
             return u;
         }
         for k in (0..self.parent.len()).rev() {
             if self.parent[k][u] != self.parent[k][v] {
-                u = self.parent[k][u];
-                v = self.parent[k][v];
+                u = self.parent[k][u].unwrap();
+                v = self.parent[k][v].unwrap();
             }
         }
-        self.parent[0][u]
+        self.parent[0][u].unwrap()
     }
 }
 
