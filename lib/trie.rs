18ae5775ad6c07eb2db73b15ff7c19564e080cb7
fukatani <nannyakannya@gmail.com>
2020-08-10 23:37:31 +0900

    trie generic.

--- lib/trie.rs
+++ lib/trie.rs
@@ -1,12 +1,21 @@
-#[derive(Debug)]
-struct TrieNode {
-    ch: char,
+use std::hash::Hash;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+struct TrieNode<K>
+where
+    K: Eq + Hash + Clone + Copy,
+{
+    ch: K,
     count: usize,
-    children: HashMap<char, TrieNode>,
+    children: HashMap<K, TrieNode<K>>,
 }
 
-impl TrieNode {
-    fn with_char(ch: char) -> TrieNode {
+impl<K> TrieNode<K>
+where
+    K: Eq + Hash + Clone + Copy,
+{
+    fn with_char(ch: K) -> TrieNode<K> {
         TrieNode {
             ch: ch,
             count: 0,
@@ -15,20 +24,31 @@ impl TrieNode {
     }
 }
 
-#[derive(Debug)]
-struct Trie {
-    node: TrieNode,
+#[derive(Debug, Clone)]
+struct Trie<K>
+where
+    K: Eq + Hash + Clone + Copy,
+{
+    node: TrieNode<K>,
+    init: K,
 }
 
-impl Trie {
-    fn new() -> Trie {
-        let node = TrieNode::with_char('#');
-        Trie { node: node }
+impl<K> Trie<K>
+where
+    K: Eq + Hash + Clone + Copy,
+{
+    fn new(init: K) -> Trie<K> {
+        let node = TrieNode::with_char(init);
+        Trie {
+            node: node,
+            init: init,
+        }
     }
 
-    fn add_word(&mut self, word: &[char]) {
+    fn add_word(&mut self, word: &[K]) {
         let mut cur = &mut self.node;
         for &ch in word {
+            assert!(self.init != ch);
             if !cur.children.contains_key(&ch) {
                 let node = TrieNode::with_char(ch);
                 cur.children.insert(ch, node);
@@ -38,7 +58,7 @@ impl Trie {
         cur.count += 1;
     }
 
-    fn count(&self, word: &[char]) -> usize {
+    fn count(&self, word: &[K]) -> usize {
         let mut cur = &self.node;
         for &ch in word {
             if !cur.children.contains_key(&ch) {
@@ -48,4 +68,12 @@ impl Trie {
         }
         cur.count
     }
+}
+
+fn main() {
+    let mut trie = Trie::new('#');
+    trie.add_word(&vec!['a', 'b', 'c']);
+    trie.add_word(&vec!['a', 'b', 'c']);
+    assert_eq!(trie.count(&vec!['a', 'b', 'c']), 2);
+    assert_eq!(trie.count(&vec!['a', 'b']), 0);
 }
