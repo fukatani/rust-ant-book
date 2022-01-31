bd68c333e26988f9baf7d3fc0295d9109ab60a16
Unknown <nannyakannya@gmail.com>
2022-01-31 23:34:39 +0900

    add depth2

--- lib/tree_diameter.rs
+++ lib/tree_diameter.rs
@@ -20,6 +20,42 @@ fn tree_diameter(g: &Vec<Vec<usize>>) -> usize {
     depth
 }
 
+fn dfs_depth2(
+    g: &Vec<Vec<(usize, i64)>>,
+    cur: usize,
+    parent: usize,
+    cur_cost: i64,
+    depth: &mut Vec<i64>,
+) {
+    depth[cur] = cur_cost;
+    for &(ci, cost) in g[cur].iter() {
+        if ci == parent {
+            continue;
+        }
+        dfs_depth(g, ci, cur, cur_cost + cost, depth);
+    }
+}
+
+fn tree_diameter2(g: &Vec<Vec<(usize, i64)>>) -> (usize, usize) {
+    let mut depth = vec![0; g.len()];
+    dfs_depth(&g, 0, g.len(), 0, &mut depth);
+    let mut vertex1 = 0;
+    for i in 0..g.len() {
+        if depth[vertex1] < depth[i] {
+            vertex1 = i;
+        }
+    }
+    let mut depth = vec![0; g.len()];
+    dfs_depth(&g, vertex1, g.len(), 0, &mut depth);
+    let mut vertex2 = 0;
+    for i in 0..g.len() {
+        if depth[vertex2] < depth[i] {
+            vertex2 = i;
+        }
+    }
+    (vertex1, vertex2)
+}
+
 fn main() {
     let mut g = vec![vec![]; 6];
     let pairs = vec![(1, 2), (2, 3), (2, 4), (4, 6), (5, 6)];
