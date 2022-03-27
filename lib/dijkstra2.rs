dedba64596d4c767a83bc2bfd062800e64728d08
Unknown <nannyakannya@gmail.com>
2022-03-27 21:19:19 +0900

    dijkstra

--- lib/dijkstra2.rs
+++ lib/dijkstra2.rs
@@ -1,35 +1,71 @@
 type Cost = i64;
 const INF: Cost = 100000_00000_00000;
 
-fn solve(edges: &Vec<Vec<(usize, Cost)>>, start_idx: usize) -> Vec<Cost> {
+fn solve(edges: &Vec<Vec<(usize, Cost)>>, start_idx: usize) -> (Vec<Cost>, Vec<Option<usize>>) {
     let num_apexes = edges.len();
     let mut d = vec![INF; num_apexes];
+    let mut from = vec![None; num_apexes];
     d[start_idx] = 0;
     let mut que = std::collections::BinaryHeap::new();
     que.push((std::cmp::Reverse(0), start_idx));
 
-    while let Some((u, v)) = que.pop() {
-        if d[v] < u.0 {
+    while let Some((cur_cost, cur)) = que.pop() {
+        if d[cur] < cur_cost.0 {
             continue;
         }
-        for e in &edges[v] {
-            if d[v] != INF && d[e.0] > d[v] + e.1 {
-                d[e.0] = d[v] + e.1;
-                que.push((std::cmp::Reverse(d[e.0]), e.0));
+        for &(to, cost) in &edges[cur] {
+            if d[cur] != INF && d[to] > d[cur] + cost {
+                d[to] = d[cur] + cost;
+                from[to] = Some(cur);
+                que.push((std::cmp::Reverse(d[to]), to));
             }
         }
     }
-    d
+    (d, from)
 }
 
+// https://judge.yosupo.jp/problem/shortest_path
 fn main() {
-    let mut edges = vec![Vec::new(); 6];
-    edges[0].push((1, 5));
-    edges[0].push((2, 4));
-    edges[1].push((2, 2));
-    edges[2].push((3, 2));
-    edges[2].push((4, 1));
-    edges[2].push((5, 4));
-    edges[4].push((5, 4));
-    solve(&edges, 0);
+    let v = read_vec::<usize>();
+    let (n, m, s, t) = (v[0], v[1], v[2], v[3]);
+    let mut edges = vec![vec![]; n];
+    for i in 0..m {
+        let v = read_vec::<i64>();
+        let (a, b, c) = (v[0] as usize, v[1] as usize, v[2]);
+        edges[a].push((b, c));
+    }
+    let (d, from) = solve(&edges, s);
+    if d[t] == INF {
+        println!("-1");
+        return;
+    }
+    let mut path = vec![t];
+    let mut cur = t;
+    while cur != s {
+        cur = from[cur].unwrap();
+        path.push(cur);
+    }
+    path.reverse();
+    println!("{} {}", d[t], path.len() - 1);
+    for i in 1..path.len() {
+        println!("{} {}", path[i - 1], path[i]);
+    }
+}
+
+use std::cmp::*;
+use std::collections::*;
+use std::io::Write;
+use std::ops::Bound::*;
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
 }
