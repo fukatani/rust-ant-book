e1f7d60225e794435dd57f892085cab7e12cf628
Unknown <nannyakannya@gmail.com>
2022-09-18 22:18:40 +0900

    rename

--- lib/slide_minimum.rs
+++ lib/slide_minimum.rs
@@ -2,20 +2,20 @@
 use std::cmp::*;
 use std::collections::*;
 
-fn slide_minimum(a: &Vec<i64>, k: usize) -> Vec<i64> {
-    assert!(k <= a.len());
+fn slide_minimum(a: &Vec<i64>, width: usize) -> Vec<i64> {
+    assert!(width <= a.len());
 
     let mut deq = VecDeque::new();
-    let mut ans = vec![0; a.len() - k + 1];
+    let mut ans = vec![0; a.len() - width + 1];
     for i in 0..a.len() {
         while !deq.is_empty() && a[deq[deq.len() - 1]] >= a[i] {
             deq.pop_back();
         }
         deq.push_back(i);
 
-        if i + 1 >= k {
-            ans[i + 1 - k] = a[deq[0]];
-            if deq[0] == i + 1 - k {
+        if i + 1 >= width {
+            ans[i + 1 - width] = a[deq[0]];
+            if deq[0] == i + 1 - width {
                 deq.pop_front();
             }
         }
