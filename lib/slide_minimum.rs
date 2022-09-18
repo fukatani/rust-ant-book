a6776756a88ab966da021a9fc8556c667661e7a2
Unknown <nannyakannya@gmail.com>
2022-09-18 22:14:01 +0900

    update slide minimum

--- lib/slide_minimum.rs
+++ lib/slide_minimum.rs
@@ -4,22 +4,19 @@ use std::collections::*;
 
 fn slide_minimum(a: &Vec<i64>, k: usize) -> Vec<i64> {
     assert!(k <= a.len());
-    let mut s = 0;
-    let mut t = 0;
-    let mut deq = vec![0; a.len()];
+
+    let mut deq = VecDeque::new();
     let mut ans = vec![0; a.len() - k + 1];
     for i in 0..a.len() {
-        while s < t && a[deq[t - 1]] >= a[i] {
-            t -= 1;
+        while !deq.is_empty() && a[deq[deq.len() - 1]] >= a[i] {
+            deq.pop_back();
         }
-        deq[t] = i;
-        t += 1;
-        // println!("{:?}", &deq[s..t]);
+        deq.push_back(i);
 
         if i + 1 >= k {
-            ans[i + 1 - k] = a[deq[s]];
-            if deq[s] == i + 1 - k {
-                s += 1;
+            ans[i + 1 - k] = a[deq[0]];
+            if deq[0] == i + 1 - k {
+                deq.pop_front();
             }
         }
     }
