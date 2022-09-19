05babb44bc78c5aabf621e5a9036468bdb4144ea
Unknown <nannyakannya@gmail.com>
2022-09-19 12:08:51 +0900

    slide minimum struct

--- lib/slide_minimum.rs
+++ lib/slide_minimum.rs
@@ -2,6 +2,32 @@
 use std::cmp::*;
 use std::collections::*;
 
+#[derive(Default, Debug)]
+struct SlideMinimum {
+    deq: VecDeque<(usize, i64)>,
+}
+
+impl SlideMinimum {
+    fn push(&mut self, index: usize, value: i64) {
+        while !self.deq.is_empty() && self.deq[self.deq.len() - 1].1 >= value {
+            self.deq.pop_back();
+        }
+        self.deq.push_back((index, value));
+    }
+
+    fn get(&mut self, cur: usize, width: usize) -> i64 {
+        while cur - self.deq[0].0 >= width {
+            self.deq.pop_front();
+        }
+        assert!(!self.deq.is_empty());
+        self.deq[0].1
+    }
+
+    fn clear(&mut self) {
+        self.deq.clear();
+    }
+}
+
 fn slide_minimum(a: &Vec<i64>, width: usize) -> Vec<i64> {
     assert!(width <= a.len());
 
