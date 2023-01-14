49f953c7d9cb2dd5584c59d02a4c5df46724c89c
Unknown <nannyakannya@gmail.com>
2023-01-14 20:19:44 +0900

    h

--- lib/rolling_hash.rs
+++ lib/rolling_hash.rs
@@ -1,17 +1,19 @@
-const BASE: u64 = 1e9 as u64 + 7;
-const MOD: u64 = 1e9 as u64 + 9;
+use std::num::Wrapping;
+
+const BASE: Wrapping<u64> = Wrapping(1009);
+const MOD: Wrapping<u64> = Wrapping(1e9 as u64 + 7);
 
 pub struct RollingHash {
-    hash: Vec<u64>,
-    pow: Vec<u64>,
+    hash: Vec<Wrapping<u64>>,
+    pow: Vec<Wrapping<u64>>,
 }
 
 impl RollingHash {
-    pub fn new(s: &[u64], base: u64) -> RollingHash {
+    pub fn new(s: &[Wrapping<u64>], base: Wrapping<u64>) -> RollingHash {
         let n = s.len();
-        let mut hash: Vec<u64> = vec![0; n + 1];
-        let mut pow: Vec<u64> = vec![0; n + 1];
-        pow[0] = 1;
+        let mut hash: Vec<Wrapping<u64>> = vec![Wrapping(0); n + 1];
+        let mut pow: Vec<Wrapping<u64>> = vec![Wrapping(0); n + 1];
+        pow[0] = Wrapping(1);
         for i in 0..n {
             pow[i + 1] = (pow[i] * base) % MOD;
             hash[i + 1] = (hash[i] * base + s[i]) % MOD;
@@ -23,7 +25,7 @@ impl RollingHash {
     }
 
     /// Get hash of [l, r)
-    pub fn get_hash(&self, l: usize, r: usize) -> u64 {
+    pub fn get_hash(&self, l: usize, r: usize) -> Wrapping<u64> {
         (self.hash[r] + MOD - (self.hash[l] * self.pow[r - l]) % MOD) % MOD
     }
 }
